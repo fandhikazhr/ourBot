346fa939b28f33833ecb8d3b3b3a302b0a95cfae
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-03-27 02:23:13 +0700

    Update telegram.ino

--- telegram.ino
+++ telegram.ino
@@ -4,3 +4,9 @@ CTBot myBot;
 String ssid = ""; // your wifi ssid 
 String pass = ""; // your wifi password
 String token = ""; // fill this with your bot token
+const int id = ; // id your telegram account
+
+void setup() {
+    Serial.begin(9600);
+    myBot.wifiConnect(ssid, pass);
+    myBot.setTelegramToken(token);
