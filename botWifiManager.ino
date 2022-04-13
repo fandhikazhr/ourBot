c0c8dd126dec4fbc41f844315f0cdb3de8c6f1de
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-04-14 04:38:51 +0700

    Update botWifiManager.ino

--- botWifiManager.ino
+++ botWifiManager.ino
@@ -4,3 +4,22 @@
 CTBot myBot; 
 String token = ""; // fill with your bot token
 const int id = ; // fill with your id
+void setup() {
+    WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+    // it is a good practice to make sure your code sets wifi mode how you want it.
+
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    myBot.setTelegramToken(token);
+
+    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wm;
+
+    // reset settings - wipe stored credentials for testing
+    // these are stored by the esp library
+    //wm.resetSettings();
+
+    // Automatically connect using saved credentials,
+    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
+    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
+    // then goes into a blocking loop awaiting configuration and will return success result
