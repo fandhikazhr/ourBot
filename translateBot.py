f521494866bed543aba43eda59631984cebad2fb
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-08-12 20:51:59 +0700

    Update translateBot.py

--- translateBot.py
+++ translateBot.py
@@ -6,3 +6,16 @@ from pprint import pprint
 
 api = 'YOUR TOKEN BOT'
 bot = telebot.TeleBot(api)
+
+translator = Translator()
+
+@bot.message_handler(commands=['start'])
+def selamat_datang(message):
+    #bot.reply_to(message, 'Selamat Datang!')
+    markup = types.ReplyKeyboardMarkup(row_width=1)
+    btn1 = types.KeyboardButton('/help')
+    btn2 = types.KeyboardButton('/about')
+    btn3 = types.KeyboardButton('/info')
+    markup.add(btn1, btn2, btn3)
+    chatid = message.chat.id
+    bot.send_message(chatid, text='', reply_markup=markup)
