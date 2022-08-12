142fcd7f0ea653d8562daa3517516da88b000681
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-08-12 20:49:55 +0700

    Update translateBot.py

--- translateBot.py
+++ translateBot.py
@@ -3,3 +3,6 @@ from telebot import types
 from datetime import datetime
 from googletrans import Translator, constants
 from pprint import pprint
+
+api = 'YOUR TOKEN BOT'
+bot = telebot.TeleBot(api)
