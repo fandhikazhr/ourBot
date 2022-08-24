5904e481845e7bba4876c2e8def4de711750566b
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-08-24 18:08:36 +0700

    Update telebot.ino

--- telebot.ino
+++ telebot.ino
@@ -1,3 +1,7 @@
 #include "CTBot.h"
 #include <WiFiUdp.h>
 #include <NTPClient.h>
+
+CTBot myBot;
+CTBotReplyKeyboard myKbd;   // reply keyboard object helper
+bool isKeyboardActive;      // store if the reply keyboard is shown
