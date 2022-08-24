f076e847a6fd7e8c26e8022baeb59ddfd0052517
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-08-24 18:11:29 +0700

    Update telebot.ino

--- telebot.ino
+++ telebot.ino
@@ -16,3 +16,41 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org");
 
 String weekDays[7]={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
 String months[12]={"January","February","March","April","May","June","July","August","September","October","November","December"};
+
+void setup() {
+  // initialize the Serial
+  Serial.begin(115200);
+  Serial.println("Starting TelegramBot...");
+
+  // connect the ESP8266 to the desired access point
+  myBot.wifiConnect(ssid, pass);
+
+  // set the telegram bot token
+  myBot.setTelegramToken(token);
+
+  // check if all things are ok
+  if (myBot.testConnection())
+    Serial.println("\ntestConnection OK");
+  else
+    Serial.println("\ntestConnection FAILED");
+
+  // reply keyboard customization
+  // add a button that send a message with "Simple button" text
+  myKbd.addButton("Time");
+  // add another button that send the user contact
+  myKbd.addButton("Contact request", CTBotKeyboardButtonContact);
+  // add another button that send the user position (location)
+  myKbd.addButton("Location request", CTBotKeyboardButtonLocation);
+  // add a new empty button row
+  myKbd.addRow();
+  // add a button that send a message with "Hide replyKeyboard" text
+  // (it will be used to hide the reply keyboard)
+  myKbd.addButton("Hide replyKeyboard");
+  // resize the keyboard to fit only the needed space
+  myKbd.enableResize();
+  isKeyboardActive = false;
+
+  // Initial NTPClient to get time
+  timeClient.begin();
+  timeClient.setTimeOffset(0);
+}
