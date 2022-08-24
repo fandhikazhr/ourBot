e048b4ff5b8c01c56409b4b37fa89e106d6ec158
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-08-24 18:10:03 +0700

    Update telebot.ino

--- telebot.ino
+++ telebot.ino
@@ -5,3 +5,8 @@
 CTBot myBot;
 CTBotReplyKeyboard myKbd;   // reply keyboard object helper
 bool isKeyboardActive;      // store if the reply keyboard is shown
+
+String ssid = ""; // fill with yours ssid
+String pass = ""; // fill with yours password of ssid
+String token = ""; // filll with yours token bot
+
