b0f83be81129a0c660a1740273cb4bc5ff06c291
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-10-15 17:33:28 +0700

    Update feedingFish.ino

--- feedingFish.ino
+++ feedingFish.ino
@@ -7,3 +7,10 @@ String ssid = "YOUR'S WIFI SSID";
 String pass = "YOUR'S WIFI PASSWORD";
 String token = "YOUR'S BOT TOKEN";
 const int id = ; //YOUR'S TELEGRAM ID
+
+void setup(){
+  Serial.begin(9600);
+  myServo.attach(0); // signal pin on NodeMCU ESP8266 at D3
+  myBot.wifiConnect(ssid, pass);
+  myBot.setTelegramToken(token);
+}
